9f513dcb2738ef3bb7efec79cf25cac0760be09a
Igor Tokarev <igor.tokarev@outlook.com>
2022-03-29 19:16:46 +0200

    add external Tailwind CSS

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import logo from "../assets/images/dev-logo.png"
+
+function Header() {
+
+  return(
+    <div>
+      <img src={logo} width={"100"} alt="Logo" />
+      <br/>
+    </div>
+  )
+}
+
+export default Header;
