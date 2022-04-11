64d1618343272146eade72b9b466ec13380347bb
Igor Tokarev <igor.tokarev@outlook.com>
2022-04-11 21:15:27 +0200

    movie details page styling #

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,7 +6,6 @@ import styles from "./Movie.module.scss"
 function Movie() {
 
   let { movieId } = useParams();
-  console.log(movieId)
   const { REACT_APP_API_KEY } = process.env;
   const [movie, setMovie] = useState([]);
 
@@ -25,47 +24,49 @@ function Movie() {
     }
   },[movieId, REACT_APP_API_KEY])
 
+   const getYear = (y) => {
+    return y.slice(0, 4)
+   };
+
   console.log(movie)
 
   return(
-    <div class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
-      <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
-        <div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
-          <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Beach House in Collingwood</h1>
-          <p class="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Entire house</p>
-      </div>
-      <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2   lg:row-end-6 lg:row-span-6 lg:mb-0">
-          <img src="/beach-house.jpg" alt="" class="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy"/>
-          <img src="/beach-house-interior-1.jpg" alt="" class="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
-          <img src="/beach-house-interior-2.jpg" alt="" class="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
-      </div>
-        <div class="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
-          <dt class="sr-only">Reviews</dt>
-          <div class="text-indigo-600 flex items-center dark:text-indigo-400">
-            <svg width="24" height="24" fill="none" aria-hidden="true" class="mr-1 stroke-current dark:stroke-indigo-500">
-              <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
-            <span>4.89 <span class="text-slate-400 font-normal">(128)</span></span>
-          </div>
-          <div class="sr-only">Location</div>
-          <div class="flex items-center">
-            <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-3 text-slate-300">
-              <circle cx="1" cy="1" r="1" />
-            </svg>
-            <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
-              <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
-              <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
-            </svg>
-            Collingwood, Ontario
+    <div className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+      <div className="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+        <div className="flex-col p-3 col-start-1 row-start-1 flex rounded-lg sm:row-start-2 sm:p-0 lg:row-start-1">
+          <h1 className="mt-1 text-2xl font-semibold sm:text-slate-900 md:text-2xl ">{movie.original_title}</h1>
+          <div className="mt-2 text-sm leading-4 font-medium sm:text-slate-500 dark:sm:text-slate-400">{movie.release_date}</div>
+          <div className=" mt-4 text-sm leading-4 font-medium sm:text-slate-500 dark:sm:text-slate-400">
+            {movie?.genres?.map((genre) => {
+              return(<span className="mr-4">{genre.name}</span>)
+            })}
           </div>
         </div>
-        <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
-          <button type="button" class="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Check availability</button>
+        <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2   lg:row-end-6 lg:row-span-6 lg:mb-0">
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="" className=" h-60 object-fit rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy"/>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy"/>
         </div>
-        <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
-          This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.
+      
+
+        <div className="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
+        
+          <div className="sr-only">Reviews</div>
+          <div className="text-indigo-600 flex items-center dark:text-indigo-400">
+            <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
+              <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  Stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <span>{movie.vote_average} <span className="text-slate-400 font-normal">(128)</span></span>
+          </div>
+          <div className="sr-only">Location</div>
+        </div>
+        <div className="mt-4 col-start-1 row-start-5 self-center sm:mt-0 sm:col-start-2 sm:row-start-5 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-5 lg:row-end-6">
+          <button type="button" className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Read more</button>
+        </div>
+        <p className="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+          {movie.overview}
         </p>
-      </div>
+     </div>
     </div>
 
   )
